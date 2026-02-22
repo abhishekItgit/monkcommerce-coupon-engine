3f81c93aa340d1a0410dd06ca9a1f3b099fae712
abhishekItgit <mrabhishekdwivediit@gmail.com>
2026-02-22 15:54:47 +0530

    feat: introduce coupon engine service for strategy orchestration

---
+++ coupon/src/main/java/com/monkcommerce/coupon/engine/CouponEngineService.java
@@ -0,0 +1,68 @@
+package com.monkcommerce.coupon.engine;
+
+import com.monkcommerce.coupon.dto.*;
+import com.monkcommerce.coupon.factory.CouponStrategyFactory;
+import com.monkcommerce.coupon.model.Coupon;
+import com.monkcommerce.coupon.repository.CouponRepository;
+import com.monkcommerce.coupon.strategy.CouponStrategy;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CouponEngineService {
+
+    private final CouponRepository couponRepository;
+    private final CouponStrategyFactory strategyFactory;
+
+    public CouponEngineService(
+            CouponRepository couponRepository,
+            CouponStrategyFactory strategyFactory) {
+
+        this.couponRepository = couponRepository;
+        this.strategyFactory = strategyFactory;
+    }
+
+    // GET all applicable coupons
+    public List<ApplicableCouponResponse> getApplicableCoupons(CartRequest cart) {
+
+        List<Coupon> coupons = couponRepository.findAll();
+
+        List<ApplicableCouponResponse> result = new ArrayList<>();
+
+        for (Coupon coupon : coupons) {
+
+            if (coupon.getActive() == null || !coupon.getActive()) continue;
+
+            CouponStrategy strategy =
+                    strategyFactory.getStrategy(coupon.getType());
+
+            if (strategy == null) continue;
+
+            ApplicableCouponResponse response =
+                    strategy.checkApplicable(coupon, cart);
+
+            if (response != null) {
+                result.add(response);
+            }
+        }
+
+        return result;
+    }
+
+    // APPLY specific coupon
+    public ApplyCouponResponse applyCoupon(Long id, CartRequest cart) {
+
+        Coupon coupon = couponRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Coupon not found"));
+
+        CouponStrategy strategy =
+                strategyFactory.getStrategy(coupon.getType());
+
+        if (strategy == null)
+            throw new RuntimeException("Strategy not found");
+
+        return strategy.applyCoupon(coupon, cart);
+    }
+}
